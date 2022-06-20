ecafdd847cdea50d32366ef5e61cac4b5ae802eb
Ish Thumber <80597098+IshThumber@users.noreply.github.com>
2022-06-20 23:16:15 +0530

    styled

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -8,11 +8,39 @@ import { FaGithub, FaLinkedinIn, FaInstagram } from "react-icons/fa";
 const nav = () => {
   return (
     <div className="navbar">
-      <div className="nav">ishthumber.me</div>
+      <div className="nav">
+        <a href="">
+          ishthumber.me
+        </a>
+      </div>
       <div className="icons">
-        <a className="instagram" href="https://www.linkedin.com/in/ishthumber/" target="_blank" rel="noopener noreferrer"><FaInstagram color="grey" fontSize="1.5rem" /></a>
-        <a className="linkedin" href="https://www.linkedin.com/in/ishthumber/" target="_blank" rel="noopener noreferrer"><FaLinkedinIn color="grey" fontSize="1.5rem" /></a>
-        <a className="github" href="https://github.com/IshThumber" target="_blank" rel="noopener noreferrer"><FaGithub color="grey" fontSize="1.5rem" /></a>
+        <a
+          className="instagram"
+          href="https://www.linkedin.com/in/ishthumber/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <FaInstagram
+            color="grey"
+            fontSize="1.5rem"
+          /></a>
+        <a
+          className="linkedin"
+          href="https://www.linkedin.com/in/ishthumber/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <FaLinkedinIn
+            color="grey"
+            fontSize="1.5rem"
+          /></a>
+        <a
+          className="github"
+          href="https://github.com/IshThumber"
+          target="_blank"
+          rel="noopener noreferrer">
+          <FaGithub
+            color="grey"
+            fontSize="1.5rem"
+          /></a>
       </div>
     </div>
   )
