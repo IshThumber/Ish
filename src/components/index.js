aa62259b085fac8ac8864edda53162d6c969907d
Ish Thumber <80597098+IshThumber@users.noreply.github.com>
2022-06-20 19:15:12 +0530

    yarn installed

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as About } from './about';
-// export { default as Contact } from './contact';
-export { default as Header } from './header';
-export { default as Nav } from './nav';
-export { default as Projects } from './projects';
-export { default as Footer } from './footer';
