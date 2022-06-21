bc2749258f1347d9abbbd0b4d9a0a0625394b18a
Ish Thumber <80597098+IshThumber@users.noreply.github.com>
2022-06-21 22:39:29 +0530

    deleted everything

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Nav } from "../components/Nav/nav.js"
-export { default as Header } from "../components/Header/header.js"
-export { default as About } from "../components/About/about.js"
-export { default as Projects } from "../components/Projects/projects.js"
-export { default as Contact } from "../components/Contact/contact.js"
-export { default as Footer } from "../components/Footer/footer.js"
